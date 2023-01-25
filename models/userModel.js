99bf395c106a3999f7c179d65c6e4daa180c2aa5
fasilkp <fasilkp314@gmail.com>
2023-01-25 09:41:19 +0530

    user model created

---
+++ models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+const userSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    ban:{
+        type:Boolean,
+        default:false
+    },
+    address:{
+        type:Array,
+        default:[]
+    },
+    cart:{
+        type:Array,
+        default:[]
+    },
+    wishlist:{
+        type:Array,
+        default:[]
+    }
+})
+const UserModel= mongoose.model("users", userSchema);
+export default UserModel;
